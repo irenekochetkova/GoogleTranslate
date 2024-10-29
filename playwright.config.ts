eb624038c11df4c65945e2061933152aaa5e1851
Irina <irina@victors-MacBook-Pro.local>
2024-10-29 19:22:19 +0300

    add reporter congig

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,9 +5,10 @@ export default defineConfig({
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  // reporter: 'html',
-  // reporter: [["line"], ["allure-playwright"]],
-  reporter: [["allure-playwright"]],
+  reporter: [['json', { outputFile: 'test-results/jsonReport.json' }],
+  ['junit', { outputFile: 'test-results/junitReport.xml' }],
+  // ['allure-playwright'],
+  ['html']],
   use: {
     channel: 'chrome',
     baseURL: 'https://translate.google.com/',
